b6dc0532dbc83d6b60dcd46bd8ee6268f71349c7
Rene Avila <reneavila1993@gmail.com>
2020-05-11 00:25:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,45 +2,56 @@
 require 'yaml'
 require 'pry'
 
-def load_library(yaml)
- emoticons = YAML.load_file(yaml) 
- output = {}
- emoticons.each do |name, emoticon|
-    english = emoticon[0]
-    japanese = emoticon[1]
-    output[name] = {}
-    output[name] = {:english =>english, :japanese => japanese}
- #binding.pry
-  end
- output
- end
+# def load_library(yaml)
+# emoticons = YAML.load_file(yaml) 
+# output = {}
+# emoticons.each do |name, emoticon|
+#     english = emoticon[0]
+#     japanese = emoticon[1]
+#     output[name] = {}
+#     output[name] = {:english =>english, :japanese => japanese}
+# #binding.pry
+#   end
+# output
+# end
 
-def get_english_meaning(yaml, emoji)
-  emoticons = load_library(yaml)
-  #binding.pry
-  result = nil
-  emoticons.each do |key, value|
-    #binding.pry
-    if emoticons[key].values.include?(emoji)
-      result = key
-      break
-    else
-      result =  "Sorry, that emoticon was not found"
-    end
-  end
-  result
-end
+# def get_english_meaning(yaml, emoji)
+#   emoticons = load_library(yaml)
+#   #binding.pry
+#   result = nil
+#   emoticons.each do |key, value|
+#     #binding.pry
+#     if emoticons[key].values.include?(emoji)
+#       result = key
+#       break
+#     else
+#       result =  "Sorry, that emoticon was not found"
+#     end
+#   end
+#   result
+# end
 
-def get_japanese_emoticon(yaml, emoji)
-  emoticons = load_library(yaml)
-  result = nil
+# def get_japanese_emoticon(yaml, emoji)
+#   emoticons = load_library(yaml)
+#   result = nil
+#   emoticons.each do |key, value|
+#     if emoticons[key].values.include?(emoji)
+#       result = value[:japanese]
+#       break
+#     else
+#       result = "Sorry, that emoticon was not found"
+#     end
+#   end
+#   result
+# end
+
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+  output = {}
   emoticons.each do |key, value|
-    if emoticons[key].values.include?(emoji)
-      result = value[:japanese]
-      break
-    else
-      result = "Sorry, that emoticon was not found"
-    end
+    output[key] = {}
+    output[key][:english] = value[0]
+    output[key][:japanese] = value[1]
   end
-  result
+  output
 end
